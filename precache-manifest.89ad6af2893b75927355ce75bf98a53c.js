5fedec9e1087b17b8e9aee5b7833fd4e6428638a
Abotzvi Yodgarov <Abotzvi.Yadgarov@gmail.com>
2020-05-04 14:29:03 +0300

    Updates

---
+++ precache-manifest.89ad6af2893b75927355ce75bf98a53c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a0a86f91dbb5f84bafca5c4930ab0f",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "448e6582b9107165d84e",
+    "url": "/weatherApp/static/css/main.e5f01987.chunk.css"
+  },
+  {
+    "revision": "6e190b559ae96fb49d48",
+    "url": "/weatherApp/static/js/2.f50f6212.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherApp/static/js/2.f50f6212.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "448e6582b9107165d84e",
+    "url": "/weatherApp/static/js/main.9f6b6680.chunk.js"
+  },
+  {
+    "revision": "d1c6777fd85a5e2c40e0",
+    "url": "/weatherApp/static/js/runtime-main.041819a9.js"
+  }
+]);
